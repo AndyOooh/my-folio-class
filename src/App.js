3eb9866aa5505c7343c4134e25d907288fa731a4
Andy Ohhh <andy.servedbynature@gmail.com>
2020-08-19 01:17:06 +0700

    added .env file with env variables for sensitive data, included in .gitognore, also made a test.env and did not include it. Seems I've learned how to handle sensitive data in development in React.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
+const cmcApiKey = process.env.REACT_APP_cmc_api_key;
+
+const cmcApiUrl = 'https://pro-api.coinmarketcap.com?';
+
+const endpoint = 'CMC_PRO_API_KEY';
+
+console.log('cmcApiKey', typeof cmcApiKey, cmcApiKey);
+
 class App extends React.Component {
+
   render() {
-    return <div>Div in App comp</div>;
+    return <div>Div in App comp
+      <div></div>
+    </div>;
   }
 }
 
